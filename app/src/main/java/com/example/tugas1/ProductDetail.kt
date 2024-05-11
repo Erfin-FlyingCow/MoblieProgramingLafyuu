e5f857c0e6b965bcca79ed547182dbabe5ea3f9a
Erfin-FlyingCow <noufalirfan02@gmail.com>
2024-05-11 22:53:25 +0700

    More review screen clear

--- app/src/main/java/com/example/tugas1/ProductDetail.kt
+++ app/src/main/java/com/example/tugas1/ProductDetail.kt
@@ -3,6 +3,7 @@ package com.example.tugas1
 import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -17,9 +18,20 @@ class ProductDetail : AppCompatActivity() {
         back.setOnClickListener(){
             toHomeScreen()
         }
+        val seeMore=findViewById<TextView>(R.id.reviewseemore)
+        seeMore.setOnClickListener(){
+            goreviewSeeMore()
+        }
     }
     fun toHomeScreen() {
         Intent(this, Home::class.java).also {
+            startActivity(it)
+            finish()
+        }
+    }
+
+    fun goreviewSeeMore(){
+        Intent(this,MoreReview::class.java).also{
             startActivity(it)
             finish()
         }
