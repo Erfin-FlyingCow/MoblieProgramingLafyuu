35e3198443a8c8c763bfb83daf51c6c89ea4f7de
Erfin-FlyingCow <noufalirfan02@gmail.com>
2024-05-25 21:26:22 +0700

    Write Review Finish

--- app/src/main/java/com/example/tugas1/WriteReview.kt
+++ app/src/main/java/com/example/tugas1/WriteReview.kt
@@ -2,42 +2,30 @@ package com.example.tugas1
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.TintableBackgroundView
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MoreReview : AppCompatActivity() {
+class WriteReview : AppCompatActivity() {
 
     lateinit var back : ImageView
-    lateinit var  write: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.more_review)
+        setContentView(R.layout.writereview)
 
         back = findViewById(R.id.back)
-        write= findViewById(R.id.btnWritereview)
 
         back.setOnClickListener(){
-            goToProductDetail()
+            goreviewSeeMore()
         }
 
-        write.setOnClickListener(){
-            goToWriteReview()
-        }
     }
-
-    fun goToProductDetail() {
-        Intent(this, ProductDetail::class.java).also {
-            startActivity(it)
-            finish()
-        }
-    }
-    fun goToWriteReview() {
-        Intent(this,WriteReview::class.java).also {
+    fun goreviewSeeMore(){
+        Intent(this,MoreReview::class.java).also{
             startActivity(it)
             finish()
         }
